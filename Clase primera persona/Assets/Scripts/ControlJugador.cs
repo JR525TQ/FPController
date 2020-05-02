75f91d81cd91ec7e61a7a17c2a4e73d0224f9fb1
Francisco Chappani <33500754+JR525TQ@users.noreply.github.com>
2020-05-02 10:49:48 -0300

    inciio

---
+++ Clase primera persona/Assets/Scripts/ControlJugador.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlJugador : MonoBehaviour
+{
+    [SerializeField]
+    private float rapidezDesplazamiento = 10.0f;
+
+    public Camera camaraPrimeraPersona;
+
+    public float daño = 10.0f;
+    public float rango = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movimientoVertical = Input.GetAxis("Vertical") * rapidezDesplazamiento;
+        float movimientoHorizontal = Input.GetAxis("Horizontal") * rapidezDesplazamiento;
+
+        movimientoVertical *= Time.deltaTime;
+        movimientoHorizontal *= Time.deltaTime;
+
+        transform.Translate(movimientoHorizontal, 0, movimientoVertical);
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }            
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+
+        void Shoot()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(camaraPrimeraPersona.transform.position, camaraPrimeraPersona.transform.TransformDirection(Vector3.forward) , out hit, rango))
+            {
+                Debug.Log("hit");
+            }
+        }
+    }
+}
---
+++ Clase primera persona/Assets/Scripts/ControlMirarCamara.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlMirarCamara : MonoBehaviour
+{
+
+    public float sensibilidad;
+
+    public float suavizado = 2.0f;
+
+    Vector2 mouseMirar;
+    Vector2 suavidadV;
+
+    GameObject jugador;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        jugador = this.transform.parent.gameObject;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var md = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        md = Vector2.Scale(md, new Vector2(sensibilidad * suavizado, sensibilidad * suavizado));
+
+        suavidadV.x = Mathf.Lerp(suavidadV.x, md.x, 1f / suavizado);
+        suavidadV.y = Mathf.Lerp(suavidadV.y, md.y, 1f / suavizado);
+
+        mouseMirar += suavidadV;
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseMirar.y, Vector3.right);
+        jugador.transform.localRotation = Quaternion.AngleAxis(mouseMirar.x, jugador.transform.up);
+
+        mouseMirar.y = Mathf.Clamp(mouseMirar.y, -90f, 90f);
+    }
+}
